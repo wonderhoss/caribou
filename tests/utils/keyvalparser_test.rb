ac2f6790a6e12ab432edc640df398eedc5bcee4b
gargath <signup@lightweaver.info>
2016-01-11 20:59:02 +0000

    Change test setup code to run only once. Fixes #5.

--- tests/utils/keyvalparser_test.rb
+++ tests/utils/keyvalparser_test.rb
@@ -4,43 +4,42 @@ require 'tmpdir'
 
 class TestKeyValueParser < Minitest::Unit::TestCase
   
-  def setup
-    testFile = File.new("#{Dir.tmpdir}/philrubytest.cfg", "w+")
-    assert(File.writable?(testFile), "Unable to create test config file.")
-    testFile.puts " "
-    testFile.puts "  "
-    testFile.puts "\t"
-    testFile.puts ""
-    testFile.puts "this=isatest"
-    testFile.puts "thisis=a=test"
-    testFile.puts "  spaces =  are in this bit  "
-    testFile.puts "\ttabs\t=\tare\tin\tthis\tbit\t"
-    testFile.close
-    @testFile = testFile.path
-  end
+  testFile = File.new("#{Dir.tmpdir}/philrubytest.cfg", "w+")
+  raise IOError("Failed to create test file") unless File.writable?(testFile)
+  testFile.puts " "
+  testFile.puts "  "
+  testFile.puts "\t"
+  testFile.puts ""
+  testFile.puts "this=isatest"
+  testFile.puts "thisis=a=test"
+  testFile.puts "  spaces =  are in this bit  "
+  testFile.puts "\ttabs\t=\tare\tin\tthis\tbit\t"
+  testFile.close
+  @@testFile = testFile.path
 
-  def teardown
-    begin
-     # File.delete(@testFile)
+
+  Minitest::Unit.after_tests do
+    begin 
+     File.delete(@@testFile)
     rescue
       puts "WARN: Test config file could not be deleted."
     end
-      
   end
 
+      
   def test_file_exists
-    assert(File.exists?(@testFile), "Test config file was not created successfully.")
+    assert(File.exists?(@@testFile), "Test config file was not created successfully.")
   end
   
   def test_parser_basic
-    options = KeyValueParser.parseFile(@testFile)
+    options = KeyValueParser.parseFile(@@testFile)
     assert_kind_of Hash, options, "Parser did not return a hash."
     options.wont_be_empty
     options.size.must_equal 4, "Parser returned incorrect number of key/value pairs."
   end
   
   def test_parsed_options
-    options = KeyValueParser.parseFile(@testFile)
+    options = KeyValueParser.parseFile(@@testFile)
     options.must_include :this
     options.must_include :thisis
     options.must_include :spaces
